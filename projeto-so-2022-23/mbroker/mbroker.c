ef17627073f72c4e3b276af00c379a1092e325d7
HermitSheep <94017494+HermitSheep@users.noreply.github.com>
2023-01-02 08:44:09 +0000

    Initial commit

---
+++ projeto-so-2022-23/mbroker/mbroker.c
@@ -0,0 +1,10 @@
+#include "logging.h"
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+    fprintf(stderr, "usage: mbroker <pipename>\n");
+    WARN("unimplemented"); // TODO: implement
+    return -1;
+}
+//fuck yoou
