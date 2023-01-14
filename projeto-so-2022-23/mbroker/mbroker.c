5a1836ee65b87dc0dbc8d5aa1b9dc24fbf1076b9
Matilde Lopes <matilde.rocha.lopes@gmail.com>
2023-01-14 09:37:26 +0000

    code

--- projeto-so-2022-23/mbroker/mbroker.c
+++ projeto-so-2022-23/mbroker/mbroker.c
@@ -109,7 +109,7 @@ selector(code, session_pipe, box_name) {
 }
 
 void codeR_PUB(char *session_pipe, char *box_name) {
-	int session_fifo = open(session_pipe, O_RDONLY);
+	int session_fifo = open(session_pipe, O_WRONLY);
 	if (session_fifo == -1)  ERROR("Open session pipe failed.");
 
 	box session_box = find_box(head, box_name);
@@ -151,5 +151,90 @@ void codeR_PUB(char *session_pipe, char *box_name) {
     }
 	
 	tfs_close(box_handle);
+	close(session_fifo);
+}
+
+void codeR_SUB(char *session_pipe,char *box_name){
+	int session_fifo = open(session_pipe, O_RDONLY);
+	if (session_fifo == -1)  ERROR("Open session pipe failed.");
+
+	box session_box = find_box(head, box_name);
+    
+    if (session_box->n_subscribers <= 0) {    //need subs /sub to the same box
+        close (session_fifo);
+		return;
+    }
+	int box_handle = tfs_open(session_box->box_name, TFS_O_APPEND);
+	if(box_handle == -1) {      //box doesnt 
+        close (session_fifo);
+		return;
+    }
+	//* PRINT MESSAGE
+	size_t len = 1;
+	char line[sizeof(uint8_t) + MAX_MESSAGE];	//[ code = 10 (uint8_t) ] | [ message (char[1024]) ]
+	char *message;
+	int received_messages = 0;
+	ssize_t ret;
+	uint8_t code;
+	fcntl(session_fifo, F_SETFL, O_NONBLOCK) ;	//to have the read signal if the session pipe was closed
+	while (!server_running) {
+		//*READ
+		ret = read(session_fifo, line, sizeof(line));
+		if (ret == 0);  //*if EOF do nothing
+		else if (errno == EAGAIN) {		//session pipe was closed
+			server_running = true;
+			printf("Session pipe has been closed.");
+		}
+		else if (errno == EINTR) ERROR("Unexpected error while reading");	//read failed
+
+		//*PRINT LINE
+    	sscanf(line, "%2" SCNu8 "%s", &code, message);
+		fprintf(stdout, "%s\n", message);
+
+		received_messages++;
+	}
+
+	fprintf(stdout, "%d messages  were received.\n", received_messages);
+
+	close(session_fifo);
+	close(session_pipe);
+}
+
+void codeC_BOX(char *session_pipe, char *box_name){
+	int session_fifo = open(session_pipe, O_RDONLY);
+	if (session_fifo == -1)  ERROR("Open session pipe failed.");
+	typedef BOX *box;
+
+	//* CREATE A BOX and verify if that already exist
+	box newBox_b(char *name, uint8_t last, uint64_t box_size, uint64_t n_publishers, uint64_t n_subscribers);
+
+
+}
+
+void codeR_BOX(char *session_pipe,char *box_name){
+	int session_fifo = open(session_pipe, O_RDONLY);
+	if (session_fifo == -1)  ERROR("Open session pipe failed.");
+
+	//*REMOVE A BOX
+	int box = rmdir(box_name);		//function in c that we can remove the name of the directory to be removed
+	if (box ==0){
+		printf("Box %s remove\n", box_name);
+	}
+	else{
+		printf("Error: Unable to remove box %s\n");
+	}
+}
+
+void codeL_BOX(char *session_pipe,char * box_name){
+	int session_fifo = open(session_pipe, O_RDONLY);
+	if (session_fifo == -1)  ERROR("Open session pipe failed.");
+
+	//*PRINT LINKED LIST
+	struct box_node *head = NULL;
+	struct box_node *current = head;
+	while (current != NULL){
+		printf("%d", current->box_name);
+		current = current->next;
+	}
 	close(session_fifo);
 }
